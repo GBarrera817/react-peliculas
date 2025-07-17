6505fcd493882488297dc944c78fd789a4874a1d
Gabriela Barrera <g.barrera.angel@gmail.com>
2025-07-17 08:38:14 -0400

    Add App.module.css for component styling and update App component to use CSS modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
+// 3era forma de setear css: a partir de un css con el nombre del componente por convención: App.module.css
+import styles from './App.module.css'
+
+
 // Componente App es un componente funcional que va a retornar Interfaz de Usuario (UI)
 export default function App() {
 
@@ -5,11 +9,21 @@ export default function App() {
   const duplicar = (valor: number) => valor * 2;
   const imagenURL = '/vite.svg';
 
+  // 2da forma de establecer css a partir de un objeto y setear el atributo style en el tag con el nombre de la variable
+  const cuadradoRojo = {
+    backgroundColor: 'red',
+    width: '50px',
+    height: '50px',
+    marginLeft: '1rem'
+  }
+
   return (
     <>
-      <h1>Hola mundo</h1>
-      <h3>{subtitulo.toUpperCase()}</h3>
+      <h1 className='rojo'>Hola mundo</h1>
+      <div style={cuadradoRojo}></div>
+      <h3 className={styles.color}>{subtitulo.toUpperCase()}</h3>
       <h4>El doble de 3 es {duplicar(3)}</h4>
+      <div style={cuadradoRojo}></div>
       <img src={imagenURL} alt="logo_vite" />
     </>
   );
