89d6092b603e54c78c716b9895527065c2691e8b
Gabriela Barrera <g.barrera.angel@gmail.com>
2025-07-17 14:28:57 -0400

    Clase 42 - React Hooks: useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
 // 3era forma de setear css: a partir de un css con el nombre del componente por convención: App.module.css
 // import styles from './App.module.css'
 
+import { useState } from "react";
+
 
 // Componente App es un componente funcional que va a retornar Interfaz de Usuario (UI)
 export default function App() {
 
+  // Se ejecuta toda la funcion App cada vez que para el evento
+  console.log('renderizando el componente App')
+
+  // Hooks
+  const [texto, setTexto] = useState(''); 
+
+  let texto2 = '';
+  console.log(texto2);
+
   const manejarClick = () => alert('click');
 
-  const manejarKeyUp = (e: React.KeyboardEvent<HTMLInputElement>) => texto = e.currentTarget.value;
+  const manejarKeyUp = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    texto2 = e.currentTarget.value;
+    setTexto(e.currentTarget.value)
 
-  let texto = '';
-
+  }
   return (
     <>
       <h1 className='rojo'>Hola mundo</h1>
