0532aa0b7bf11c4b54e598fa2ba5b952ddcedcde
Gabriela Barrera <g.barrera.angel@gmail.com>
2025-07-17 08:59:29 -0400

    Clase 40: Eventos en React
    - Handling JS events through variables and event setting on HTML tags.
    - Refactor: Remove unused variables and commented-out code in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,28 @@
 // 3era forma de setear css: a partir de un css con el nombre del componente por convención: App.module.css
-import styles from './App.module.css'
+// import styles from './App.module.css'
 
 
 // Componente App es un componente funcional que va a retornar Interfaz de Usuario (UI)
 export default function App() {
 
-  const subtitulo = 'Este es otro subtitulo';
-  const duplicar = (valor: number) => valor * 2;
-  const imagenURL = '/vite.svg';
+  const manejarClick = () => alert('click');
 
-  // 2da forma de establecer css a partir de un objeto y setear el atributo style en el tag con el nombre de la variable
-  const cuadradoRojo = {
-    backgroundColor: 'red',
-    width: '50px',
-    height: '50px',
-    marginLeft: '1rem'
-  }
+  const manejarKeyUp = (e: React.KeyboardEvent<HTMLInputElement>) => texto = e.currentTarget.value;
+
+  let texto = '';
 
   return (
     <>
       <h1 className='rojo'>Hola mundo</h1>
-      <div style={cuadradoRojo}></div>
-      <h3 className={styles.color}>{subtitulo.toUpperCase()}</h3>
-      <h4>El doble de 3 es {duplicar(3)}</h4>
-      <div style={cuadradoRojo}></div>
-      <img src={imagenURL} alt="logo_vite" />
+
+      <button onClick={manejarClick}>Clickeame</button>
+
+      <div>
+        <input type="text" onKeyUp={(e) => manejarKeyUp(e)}/>
+      </div>
+      <div>
+        <p>Haz escrito: {texto}</p>
+      </div>
     </>
   );
 }
