9a5b7cc039f0d6f547a4b3ac865239e298975c18
Gabriela Barrera <g.barrera.angel@gmail.com>
2025-08-02 20:36:29 -0400

    Class 44: Pass content as parameters
    - Study form 1: as ReactNodes (ProjectContent App)
    - New way to pass the content: as parameter (in ProjectContent2 App)

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,32 @@
-// 3era forma de setear css: a partir de un css con el nombre del componente por convención: App.module.css
-// import styles from './App.module.css'
-
-import { useState } from "react";
 import Header from "./Header";
-import ShowText from "./ShowText";
+import ProjectContent from "./ProjectContent";
+import ProjectContent2 from "./ProjectContent2";
 
-
-// Componente App es un componente funcional que va a retornar Interfaz de Usuario (UI)
 export default function App() {
 
-  // Se ejecuta toda la funcion App cada vez que para el evento
-  console.log('renderizando el componente App')
-
-  // Hooks
-  const [texto, setTexto] = useState(''); 
-
-  // La variable siempre se iniciará en '', cada vez que se renderice el componente. Por eso es importante que si quiero que el valor de la variable se mantenga entre renderizados, se use el useState
-  let texto2 = '';
-  console.log(texto2);
-
-  const manejarClick = () => alert('click');
-
-  const manejarKeyUp = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    texto2 = e.currentTarget.value;
-    setTexto(e.currentTarget.value)
-
-  }
   return (
     <>
+      <h2>Ejemplo 1</h2>
+      <ProjectContent>
+        <>
+          <button onClick={() => alert('He sido clickeado')}>Clickeame</button>
+          <h4>Este es un H4</h4>
+        </>
+      </ProjectContent>
 
-      {/* Utikizar componente */}
+      <h2>Ejemplo 2</h2>
 
-      <Header/>
-
-      <button onClick={manejarClick}>Clickeame</button>
-
-      <div>
-        <input type="text" onKeyUp={(e) => manejarKeyUp(e)}/>
-      </div>
-      
-      <ShowText texto={texto} />
-
-      <Header/>
-
+      <ProjectContent2 
+        parteSuperior={<button>Botón superior</button>}
+        parteIntermedia={<>
+          
+            <Header/>
+            <p>Lo que yo quiera</p>
+        </>}
+        parteInferior={<>
+        
+        </>}
+      />
     </>
   );
 }
