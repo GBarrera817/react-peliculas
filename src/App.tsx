995e359dad49250ef553a2f91e6ec266cbca8d8e
Gabriela Barrera <g.barrera.angel@gmail.com>
2025-08-26 12:44:42 -0400

    Refactor App component to manage movie state and update MovieList rendering; handle loading and empty states in MovieList

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import MovieList from "./features/movies/components/MovieList";
 import type Movie from "./features/movies/models/movie.model";
 
@@ -9,40 +10,63 @@ export default function App() {
   //   poster: 'https://static.wikia.nocookie.net/a4aa97bb-de0c-4aab-a16c-4ab4fb1116c8'
   // }
 
-  // Features movies (en cartelera o en cines)
-  const nowPlaying: Movie[] = [
-    {
-    id: 1,
-    title: 'Sonic 3',
-    poster: 'https://static.wikia.nocookie.net/a4aa97bb-de0c-4aab-a16c-4ab4fb1116c8'
-    },
-    {
-      id: 2,
-      title: 'Mortal Kombat',
-      poster: 'https://cdn.justjared.com/wp-content/uploads/2021/04/mortal-cast/mortal-kombat-movie-photos-32.jpg'
-    }
-  ]
-  const commingSoon: Movie[] = [
-  {
-    id: 3,
-    title: 'The Long Walk',
-    poster: 'https://www.cinemark.com/media/gxfj1p52/medthelongwalkposter.jpg'
-  },
-  {
-    id: 4,
-    title: 'Dude Perfect: The Hero Tour',
-    poster: 'https://www.cinemark.com/media/gwwery0h/meddudeperfectposter.jpg'
-  }
-]
+  // Fake WEB API consuming 
+
+  const [movies, setMovies] = useState<AppState>({});
+
+  useEffect(() => {
+    setTimeout(() => {
+      // Now Playing (en cartelera o en cines)
+      const nowPlaying: Movie[] = [
+        {
+        id: 1,
+        title: 'Sonic 3',
+        poster: 'https://static.wikia.nocookie.net/a4aa97bb-de0c-4aab-a16c-4ab4fb1116c8'
+        },
+        {
+          id: 2,
+          title: 'Mortal Kombat',
+          poster: 'https://cdn.justjared.com/wp-content/uploads/2021/04/mortal-cast/mortal-kombat-movie-photos-32.jpg'
+        }
+      ];
+
+      // Comming Soon (próximos estrenos)
+      // const commingSoon: Movie[] = [
+      //   {
+      //     id: 3,
+      //     title: 'The Long Walk',
+      //     poster: 'https://www.cinemark.com/media/gxfj1p52/medthelongwalkposter.jpg'
+      //   },
+      //   {
+      //     id: 4,
+      //     title: 'Dude Perfect: The Hero Tour',
+      //     poster: 'https://www.cinemark.com/media/gwwery0h/meddudeperfectposter.jpg'
+      //   }
+      // ];
+
+      const commingSoon: Movie[] = [];
+
+      setMovies( { nowPlaying, commingSoon});
+
+    }, 1000)
+  }, []);
+
+
 
   return(
     <>
       <h3>Now Playing</h3>
-      <MovieList movies={nowPlaying} />
+      <MovieList movies={movies.nowPlaying} />
 
       <h3>Comming Soon</h3>
-      <MovieList movies={commingSoon} />
+      <MovieList movies={movies.commingSoon} />
 
     </>
   )
+}
+
+interface AppState {
+  // The ? means optional, at the begin of the execution it will be null
+  nowPlaying?: Movie[];  
+  commingSoon?: Movie[];
 }
--- src/features/movies/components/MovieList.tsx
+++ src/features/movies/components/MovieList.tsx
@@ -1,17 +1,23 @@
 import type Movie from "../models/movie.model";
 import IndividualMovie from "./IndividualMovie";
-import styles from './MovieList.module.css'
+import styles from "./MovieList.module.css";
 
 export default function MovieList(props: MovieListProps) {
-
-    return(
-        <div className={styles.div}>
-            {props.movies.map(movie => <IndividualMovie key={movie.id} movie={movie}/>)}
-        </div>
-    )
+  if (!props.movies) {
+    return "Now loading...";
+  } else if (props.movies.length === 0) {
+    return "No available movies to show";
+  } else {
+    return (
+      <div className={styles.div}>
+        {props.movies.map((movie) => (
+          <IndividualMovie key={movie.id} movie={movie} />
+        ))}
+      </div>
+    );
+  }
 }
 
 interface MovieListProps {
-    movies: Movie[];
-
+  movies?: Movie[];
 }
